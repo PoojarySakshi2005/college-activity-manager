8a0f71e727a3b25abf3d25a75e80f0bdc1934779
varsharam366-cloud <varsharam366@gmail.com>
2025-12-21 20:43:07 +0530

    Remove backend files

---
+++ activity-manager-backend/models/user.py
@@ -0,0 +1,18 @@
+from sqlalchemy import Column, Integer, String, Enum, Boolean, ForeignKey
+from database import Base
+import enum
+
+class RoleEnum(enum.Enum):
+    TEACHER = "TEACHER"
+    SR = "SR"
+
+class User(Base):
+    __tablename__ = "users"
+
+    user_id = Column(Integer, primary_key=True, index=True)
+    full_name = Column(String(100), nullable=False)
+    email = Column(String(100), unique=True, nullable=False)
+    password_hash = Column(String(255), nullable=False)
+    role = Column(Enum(RoleEnum), nullable=False)
+    is_active = Column(Boolean, default=True)
+    created_by = Column(Integer, ForeignKey("users.user_id"), nullable=True)
